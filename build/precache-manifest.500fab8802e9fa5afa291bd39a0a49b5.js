089179d920f6687280945c6299840a0dc809bd01
vineela <vinnygkakarla@gmail.com>
2020-09-01 13:39:29 +0530

    initial Commit

---
+++ build/precache-manifest.500fab8802e9fa5afa291bd39a0a49b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e24dddb5c75783b09e8edd4f892bdaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eefe903df9915f2d974",
+    "url": "/static/css/2.1b762aec.chunk.css"
+  },
+  {
+    "revision": "841c3d957963280c263c",
+    "url": "/static/css/main.181599a4.chunk.css"
+  },
+  {
+    "revision": "9eefe903df9915f2d974",
+    "url": "/static/js/2.24228d45.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/static/js/2.24228d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841c3d957963280c263c",
+    "url": "/static/js/main.4c1ac4cb.chunk.js"
+  },
+  {
+    "revision": "a577a41bd4ccf0c8d730",
+    "url": "/static/js/runtime-main.3efa262b.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
